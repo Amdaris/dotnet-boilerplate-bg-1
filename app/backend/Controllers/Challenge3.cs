b3fa3359bf24b587038caccd3878a08f9308397a
Tsanko <tsankoandreev@hotmail.com>
2025-03-04 14:10:11 +0200

    Initial commit

---
+++ app/backend/Controllers/Challenge3.cs
@@ -0,0 +1,20 @@
+﻿using backend.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    public class Challenge3 : ControllerBase
+    {
+        [HttpPost("challenge-3")]
+        public string Implementation([FromBody] RestaurantOrderRequest input)
+        {
+            string order = input.Order;
+
+            // Write the code for Challenge 3 here
+
+            return "";
+        }
+    }
+}
